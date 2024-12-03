2fe914c58f6a0dd2417189572a7522b448701024
Fedor <starbreeze@yandex.ru>
2024-12-03 03:21:53 -0800

    Catalogue Merged

--- FakeNFT/Services/Requests/NftRequest.swift
+++ FakeNFT/Services/Requests/NftRequest.swift
@@ -1,10 +1,6 @@
 import Foundation
 
-<<<<<<<< HEAD:FakeNFT/Services/Requests/NftRequest.swift
 struct NftRequest: NetworkRequest {
-========
-struct NftCollectionItemsRequest: NetworkRequest {
->>>>>>>> catalogue:FakeNFT/Services/Requests/NftCollectioItemsRequest.swift
     let id: String
     var endpoint: URL? {
         URL(string: "\(RequestConstants.baseURL)/api/v1/nft/\(id)")
